f45740cdf7dcb835697dc5a5e410cfa855dd0cf7
Ravis96 <ravis96@spoko.pl>
2023-04-04 18:07:36 +0200

    Add replaceMap support for ChatUtil

--- utilities-bukkit/src/main/java/cc/dreamcode/utilities/bukkit/ChatUtil.java
+++ utilities-bukkit/src/main/java/cc/dreamcode/utilities/bukkit/ChatUtil.java
@@ -1,11 +1,14 @@
 package cc.dreamcode.utilities.bukkit;
 
+import eu.okaeri.placeholders.context.PlaceholderContext;
+import eu.okaeri.placeholders.message.CompiledMessage;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import org.bukkit.ChatColor;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @UtilityClass
@@ -14,9 +17,25 @@ public class ChatUtil {
         return ChatColor.translateAlternateColorCodes('&', text);
     }
 
+    public static String fixColor(@NonNull String text, @NonNull Map<String, Object> replaceMap) {
+        final String colorized = fixColor(text);
+        final CompiledMessage compiledMessage = CompiledMessage.of(colorized);
+        final PlaceholderContext placeholderContext = PlaceholderContext.of(compiledMessage);
+
+        placeholderContext.with(replaceMap);
+
+        return placeholderContext.apply();
+    }
+
     public static List<String> fixColor(@NonNull List<String> stringList) {
         return stringList.stream()
                 .map(ChatUtil::fixColor)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> fixColor(@NonNull List<String> stringList, @NonNull Map<String, Object> replaceMap) {
+        return stringList.stream()
+                .map(text -> fixColor(text, replaceMap))
                 .collect(Collectors.toList());
     }
 
