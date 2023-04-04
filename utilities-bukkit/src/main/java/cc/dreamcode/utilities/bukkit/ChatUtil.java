17eff7425c07bcccf09e22063a4e5b4a3a94c6b3
Ravis96 <ravis96@spoko.pl>
2023-04-04 18:04:35 +0200

    Add fix-color method for string list

--- utilities-bukkit/src/main/java/cc/dreamcode/utilities/bukkit/ChatUtil.java
+++ utilities-bukkit/src/main/java/cc/dreamcode/utilities/bukkit/ChatUtil.java
@@ -4,9 +4,25 @@ import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import org.bukkit.ChatColor;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @UtilityClass
 public class ChatUtil {
     public static String fixColor(@NonNull String text) {
         return ChatColor.translateAlternateColorCodes('&', text);
+    }
+
+    public static List<String> fixColor(@NonNull List<String> stringList) {
+        return stringList.stream()
+                .map(ChatUtil::fixColor)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> fixColor(@NonNull String... strings) {
+        return Arrays.stream(strings)
+                .map(ChatUtil::fixColor)
+                .collect(Collectors.toList());
     }
 }
