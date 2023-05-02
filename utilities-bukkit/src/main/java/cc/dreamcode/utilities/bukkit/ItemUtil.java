b3f893451c0f9e1e6731df6aca6e1cb9a36f5f2c
eastcause <east.cause.walter@gmail.com>
2023-05-02 19:02:13 +0200

    .

--- utilities-bukkit/src/main/java/cc/dreamcode/utilities/bukkit/ItemUtil.java
+++ utilities-bukkit/src/main/java/cc/dreamcode/utilities/bukkit/ItemUtil.java
@@ -1,6 +1,7 @@
 package cc.dreamcode.utilities.bukkit;
 
 import com.google.common.collect.Lists;
+import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -14,73 +15,50 @@ import java.util.List;
 @UtilityClass
 public class ItemUtil {
 
-    public static void give(Player player, ItemStack itemStack, Location location) {
+    public static void give(@NonNull Player player, @NonNull ItemStack itemStack, @NonNull Location location) {
         give(player, Lists.newArrayList(itemStack), location);
     }
 
-    public static void give(Player player, List<ItemStack> itemStacks, Location location) {
-        if (player == null) {
-            drop(itemStacks, location);
-            return;
-        }
+    public static void give(@NonNull Player player, @NonNull List<ItemStack> itemStacks, @NonNull Location location) {
         itemStacks.forEach(itemStack -> player.getInventory().addItem(itemStack).values().forEach(noAdded -> {
-            if (location != null && location.getWorld() != null) {
-                location.getWorld().dropItem(location, noAdded);
-            }
+            location.getWorld().dropItem(location, noAdded);
         }));
     }
 
-    public static void give(Player player, ItemStack itemStack, Inventory inventory) {
+    public static void give(@NonNull Player player, @NonNull ItemStack itemStack, @NonNull Inventory inventory) {
         give(player, Lists.newArrayList(itemStack), inventory);
     }
 
-    public static void give(Player player, List<ItemStack> itemStacks, Inventory inventory) {
-        if (player == null) {
-            addItems(itemStacks, inventory);
-            return;
-        }
+    public static void give(@NonNull Player player, @NonNull List<ItemStack> itemStacks, @NonNull Inventory inventory) {
         itemStacks.forEach(itemStack -> player.getInventory().addItem(itemStack).values().forEach(noAdded -> {
-            if (inventory != null) {
-                addItem(noAdded, inventory);
-            }
+            addItem(noAdded, inventory);
         }));
     }
 
-    public static void drop(ItemStack itemStack, Location location) {
-        if (itemStack == null || location == null) {
-            return;
-        }
+    public static void drop(@NonNull ItemStack itemStack, @NonNull Location location) {
         drop(Lists.newArrayList(itemStack), location);
     }
 
-    public static void drop(List<ItemStack> itemStacks, Location location) {
-        if (itemStacks != null && location != null && location.getWorld() != null) {
-            itemStacks.forEach(itemStack -> location.getWorld().dropItem(location, itemStack));
-        }
+    public static void drop(@NonNull List<ItemStack> itemStacks, @NonNull Location location) {
+        itemStacks.forEach(itemStack -> location.getWorld().dropItem(location, itemStack));
     }
 
-    public static void addItem(ItemStack itemStack, Inventory inventory) {
-        if (itemStack == null || inventory == null) {
-            return;
-        }
+    public static void addItem(@NonNull ItemStack itemStack, @NonNull Inventory inventory) {
         addItems(Lists.newArrayList(itemStack), inventory);
     }
 
-    public static void addItems(List<ItemStack> itemStacks, Inventory inventory) {
-        if (itemStacks == null || itemStacks.size() == 0 || inventory == null) {
-            return;
-        }
+    public static void addItems(@NonNull List<ItemStack> itemStacks, @NonNull Inventory inventory) {
         itemStacks.forEach(inventory::addItem);
     }
 
-    public boolean hasSpace(Inventory inventory, ItemStack itemStack) {
+    public static boolean hasSpace(@NonNull Inventory inventory, @NonNull ItemStack itemStack) {
         Inventory newInv = Bukkit.createInventory(null, inventory.getSize());
         newInv.setContents(inventory.getContents());
 
         return newInv.addItem(itemStack.clone()).values().size() == 0;
     }
 
-    public boolean hasSpace(Inventory inventory, List<ItemStack> itemStacks) {
+    public static boolean hasSpace(@NonNull Inventory inventory, @NonNull List<ItemStack> itemStacks) {
         Inventory newInv = Bukkit.createInventory(null, inventory.getSize());
         newInv.setContents(inventory.getContents());
 
