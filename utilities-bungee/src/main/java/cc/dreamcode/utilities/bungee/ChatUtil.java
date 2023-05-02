1c89d4cb06ff675a41ce29cd3cedce0fc782393b
Ravis96 <ravis96@spoko.pl>
2023-05-02 14:14:09 +0200

    Add bungee support and bungee ChatUtil

---
+++ utilities-bungee/src/main/java/cc/dreamcode/utilities/bungee/ChatUtil.java
@@ -0,0 +1,102 @@
+package cc.dreamcode.utilities.bungee;
+
+import cc.dreamcode.utilities.builder.MapBuilder;
+import eu.okaeri.placeholders.context.PlaceholderContext;
+import eu.okaeri.placeholders.message.CompiledMessage;
+import lombok.NonNull;
+import lombok.experimental.UtilityClass;
+import net.md_5.bungee.api.ChatColor;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+@UtilityClass
+public class ChatUtil {
+
+    private static final Pattern hexPattern = Pattern.compile("&#([0-9A-Fa-f]{6})");
+    private static final Map<Color, ChatColor> COLORS = new MapBuilder<Color, ChatColor>()
+            .put(new Color(0), ChatColor.getByChar('0'))
+            .put(new Color(170), ChatColor.getByChar('1'))
+            .put(new Color(43520), ChatColor.getByChar('2'))
+            .put(new Color(43690), ChatColor.getByChar('3'))
+            .put(new Color(11141120), ChatColor.getByChar('4'))
+            .put(new Color(11141290), ChatColor.getByChar('5'))
+            .put(new Color(16755200), ChatColor.getByChar('6'))
+            .put(new Color(11184810), ChatColor.getByChar('7'))
+            .put(new Color(5592405), ChatColor.getByChar('8'))
+            .put(new Color(5592575), ChatColor.getByChar('9'))
+            .put(new Color(5635925), ChatColor.getByChar('a'))
+            .put(new Color(5636095), ChatColor.getByChar('b'))
+            .put(new Color(16733525), ChatColor.getByChar('c'))
+            .put(new Color(16733695), ChatColor.getByChar('d'))
+            .put(new Color(16777045), ChatColor.getByChar('e'))
+            .put(new Color(16777215), ChatColor.getByChar('f'))
+            .build();
+
+    public static String fixColor(@NonNull String text) {
+        return ChatColor.translateAlternateColorCodes('&', processRgb(text));
+    }
+
+    public static String fixColor(@NonNull String text, @NonNull Map<String, Object> replaceMap) {
+        final CompiledMessage compiledMessage = CompiledMessage.of(text);
+        final PlaceholderContext placeholderContext = PlaceholderContext.of(compiledMessage);
+
+        placeholderContext.with(replaceMap);
+
+        return fixColor(placeholderContext.apply());
+    }
+
+    public static List<String> fixColor(@NonNull List<String> stringList) {
+        return stringList.stream()
+                .map(ChatUtil::fixColor)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> fixColor(@NonNull List<String> stringList, @NonNull Map<String, Object> replaceMap) {
+        return stringList.stream()
+                .map(text -> fixColor(text, replaceMap))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> fixColor(@NonNull String... strings) {
+        return Arrays.stream(strings)
+                .map(ChatUtil::fixColor)
+                .collect(Collectors.toList());
+    }
+
+    private static ChatColor getClosestColor(@NonNull Color color) {
+        return COLORS.entrySet()
+                .stream()
+                .sorted(Comparator.comparing(entry -> Math.pow(color.getRed() - entry.getKey().getRed(), 2) +
+                        Math.pow(color.getRed() - entry.getKey().getRed(), 2) +
+                        Math.pow(color.getRed() - entry.getKey().getRed(), 2)))
+                .map(Map.Entry::getValue)
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("Cannot find closest rgb color to format chat-color. (" + color + ")"));
+    }
+
+    private static Color hexToRgb(@NonNull String hex) {
+        return new Color(Integer.parseInt(hex.substring(2), 16));
+    }
+
+    private static String processRgb(@NonNull String text) {
+        Matcher matcher = hexPattern.matcher(text);
+
+        AtomicReference<String> atomicText = new AtomicReference<>(text);
+        while (matcher.find()) {
+            final String hex = matcher.group();
+            final Color color = hexToRgb(hex);
+
+            atomicText.set(atomicText.get().replace(hex, ChatColor.of(color) + ""));
+        }
+
+        return atomicText.get();
+    }
+}
