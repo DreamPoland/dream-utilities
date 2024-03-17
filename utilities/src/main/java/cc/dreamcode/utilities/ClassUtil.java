98ba8f8a72aa67a6c4ade1a8010804c5f8fb2e05
Ravis96 <ravis96@spoko.pl>
2024-03-17 19:30:32 +0100

    Add getClass method to `ClassUtil`

--- utilities/src/main/java/cc/dreamcode/utilities/ClassUtil.java
+++ utilities/src/main/java/cc/dreamcode/utilities/ClassUtil.java
@@ -2,6 +2,8 @@ package cc.dreamcode.utilities;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.Optional;
+
 @UtilityClass
 public class ClassUtil {
     public static boolean hasClass(String className) {
@@ -10,6 +12,14 @@ public class ClassUtil {
             return true;
         } catch (ClassNotFoundException e) {
             return false;
+        }
+    }
+
+    public static Optional<Class<?>> getClass(String className) {
+        try {
+            return Optional.of(Class.forName(className));
+        } catch (ClassNotFoundException e) {
+            return Optional.empty();
         }
     }
 }
