2ca19c21dda068314366aac408d6498ccb7c2d47
Ravis96 <ravis96@spoko.pl>
2024-04-28 17:49:06 +0200

    Add methods to scan nbt-values without plugin key

--- utilities-bukkit/src/main/java/cc/dreamcode/utilities/bukkit/nbt/ItemNbtUtil.java
+++ utilities-bukkit/src/main/java/cc/dreamcode/utilities/bukkit/nbt/ItemNbtUtil.java
@@ -21,20 +21,28 @@ public class ItemNbtUtil {
                 : new ItemNbtLegacy();
     }
 
-    public static Map<String, String> getValues(@NonNull Plugin plugin, @NonNull ItemStack itemStack) {
+    public static Map<String, String> getValuesByPlugin(@NonNull Plugin plugin, @NonNull ItemStack itemStack) {
         return ITEM_NBT.getValues(plugin, itemStack);
     }
 
-    public static Map<String, String> getValues(@NonNull ItemStack itemStack) {
+    public static Map<String, String> getValuesByPlugin(@NonNull ItemStack itemStack) {
         return ITEM_NBT.getValues(ItemNbtUtil.plugin, itemStack);
     }
 
-    public static Optional<String> getValue(@NonNull Plugin plugin, @NonNull ItemStack itemStack, @NonNull String key) {
+    public static Map<String, String> getValues(@NonNull ItemStack itemStack) {
+        return ITEM_NBT.getValues(itemStack);
+    }
+
+    public static Optional<String> getValueByPlugin(@NonNull Plugin plugin, @NonNull ItemStack itemStack, @NonNull String key) {
         return ITEM_NBT.getValue(plugin, itemStack, key);
     }
 
-    public static Optional<String> getValue(@NonNull ItemStack itemStack, @NonNull String key) {
+    public static Optional<String> getValueByPlugin(@NonNull ItemStack itemStack, @NonNull String key) {
         return ITEM_NBT.getValue(ItemNbtUtil.plugin, itemStack, key);
+    }
+
+    public static Optional<String> getValue(@NonNull ItemStack itemStack, @NonNull String key) {
+        return ITEM_NBT.getValue(itemStack, key);
     }
 
     public static ItemStack setValue(@NonNull Plugin plugin, @NonNull ItemStack itemStack, @NonNull String key, @NonNull String value) {
