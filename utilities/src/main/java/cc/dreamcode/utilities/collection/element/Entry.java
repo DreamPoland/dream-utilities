38797b76e745c92f395e4d36444c5cf2876c4922
Ravis96 <ravis96@spoko.pl>
2024-05-18 12:04:53 +0200

    Remove lombok data annotation from Entry class

--- utilities/src/main/java/cc/dreamcode/utilities/collection/element/Entry.java
+++ utilities/src/main/java/cc/dreamcode/utilities/collection/element/Entry.java
@@ -1,18 +1,40 @@
 package cc.dreamcode.utilities.collection.element;
 
-import lombok.Data;
-
 import java.util.Map;
 import java.util.function.Consumer;
 
-@Data
 public class Entry<K, V> implements Map.Entry<K, V> {
 
     private final K key;
     private V value;
 
+    public Entry(K key) {
+        this.key = key;
+    }
+
+    public Entry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
     public Entry<K, V> then(Consumer<Entry<K, V>> after) {
         after.accept(this);
         return this;
+    }
+
+    @Override
+    public K getKey() {
+        return this.key;
+    }
+
+    @Override
+    public V getValue() {
+        return this.value;
+    }
+
+    @Override
+    public V setValue(V value) {
+        this.value = value;
+        return value;
     }
 }
